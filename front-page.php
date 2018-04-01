4b1f8d69e55ca49d90b70d141466268a25348b9d
Scott <scottdlivingstone@gmail.com>
2018-04-01 14:28:56 -0700

    Add front page template

---
+++ front-page.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+    <section class="content-wrapper">
+		  <h1><span class="uppercase">Valo</span> Smart City</h1>
+    </section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
