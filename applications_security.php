d54809dbac7c74b124fd6878b0b064a888910a5c
Sean <seanstobo@gmail.com>
2018-04-04 09:59:49 -0700

    Applications page started

--- applications_security.php
+++ applications_security.php
@@ -9,15 +9,24 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+	</div class="applications-security-wrapper">
+		<div class="applications-security-hero">
+			<div class="applications-security-img">
+			</div>
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
+			<div class="applications-security-post">
+			<h1><?php echo esc_html( get_the_title() ); ?></h1>
+			<div class="title-blue-line"></div>
+				<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php endwhile; // End of the loop. ?>
+			</div>
+		</div>
+	</div>
+	</main>
+	<!-- #main -->
+	</div>
+	<!-- #primary -->
 
-			<?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+	<?php get_footer(); ?>
--- applications_transpo.php
+++ applications_transpo.php
@@ -7,17 +7,41 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+		<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+	</div class="applications-transpo-wrapper">
+		<div class="applications-transpo-hero">
+			<div class="applications-transpo-img">
+			</div>
 
-			<?php while ( have_posts() ) : the_post(); ?>
 
+			<div class="applications-transpo-post">
+			<h1><?php echo esc_html( get_the_title() ); ?></h1>
+			<div class="title-blue-line"></div>
+				<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php endwhile; // End of the loop. ?>
+			</div>
+		</div>
+	</div>
 
-			<?php endwhile; // End of the loop. ?>
+	</div class="applications-transpo-wrapper2">
+		<div class="applications-transpo-hero2">
+			<div class="applications-transpo-img2">
+			</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			<div class="applications-transpo-post2">
+			<h1><?php echo esc_html( get_the_title() ); ?></h1>
+			<div class="title-blue-line"></div>
+				<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php endwhile; // End of the loop. ?>
+			</div>
+		</div>
+	</div>
+	</main>
+	<!-- #main -->
+	</div>
+	<!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
