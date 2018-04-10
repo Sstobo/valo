9e01f922fa07b3d5830d99cf93cd1ff3c65a00e0
Sean <seanstobo@gmail.com>
2018-04-10 07:36:23 -0700

    Applications done

--- applications_enviro.php
+++ applications_enviro.php
@@ -7,17 +7,42 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+		<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+	</div class="applications-enviro-wrapper">
+		<div class="applications-enviro-hero">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+	<div class="applications-enviro-post">
+			<h1>Smart Weather Monitoring</h1>
+			<div class="title-blue-line"></div>
+			<p>By monitoring weather patterns, cities are better prepared for extreme weather and natural disasters such as snow, rain storms, and hurricanes. Knowing changes in weather allows cities to be more proactive in dealing with weather disruptions. For example, scheduling and planning of snow removal and road salting. Valo’s Smart City Platform is ideal for hosting weather monitoring equipment since it provides a source of power, can transmit data, and can be distributed across a city.</p>
+		</div>
+	
+			<div class="applications-enviro-img">
+			<div class="applications-enviro-image"></div>
+			</div>
+		</div>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-			<?php endwhile; // End of the loop. ?>
+		
+	</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</div class="applications-enviro-wrapper2">
+		<div class="applications-enviro-hero2">
+			<div class="applications-enviro-img2">
+				<div class="applications-enviro-image2"></div>
+			</div>
 
-<?php get_sidebar(); ?>
+			<div class="applications-enviro-post2">
+			<h1>Smart Air Quality Monitoring</h1>
+			<div class="title-blue-line"></div>
+			<p>Air quality measurement allows cities to monitor pollution levels that affect citizens’ health and well-being. Street light poles are an optimal platform for mounting air quality sensors since they can be placed along city roads and spread evenly throughout a city. Sensors draw power from the light poles and relay information using communication technology in Valo’s smart street lights. This setup captures more data points, at a lower cost, compared to traditional large industrial air-sensor units.</p>
+			</div>
+		</div>
+	</div>
+	</main>
+	<!-- #main -->
+	</div>
+	<!-- #primary -->
+
 <?php get_footer(); ?>
--- applications_municipal.php
+++ applications_municipal.php
@@ -7,17 +7,42 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+		<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+	</div class="applications-muni-wrapper">
+		<div class="applications-muni-hero">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+	<div class="applications-muni-post">
+			<h1>Smart Utility Grade Metering</h1>
+			<div class="title-blue-line"></div>
+			<p>Smart meters record consumption of utilities (e.g., electricity, water, or gas) and communicate the information for monitoring or billing. Cities can improve the efficiency and reliability of utility networks using utility-grade metering. For example, a city can optimize its distribution network to reduce electrical power outages and brownouts. Valo’s Smart City Platform is well suited for utility metering since it can power smart meters and transmit information.</p>
+		</div>
+	
+			<div class="applications-muni-img">
+			<div class="applications-muni-image"></div>
+			</div>
+		</div>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-			<?php endwhile; // End of the loop. ?>
+		
+	</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</div class="applications-muni-wrapper2">
+		<div class="applications-muni-hero2">
+			<div class="applications-muni-img2">
+				<div class="applications-muni-image2"></div>
+			</div>
 
-<?php get_sidebar(); ?>
+			<div class="applications-muni-post2">
+			<h1>Smart Waste Management</h1>
+			<div class="title-blue-line"></div>
+			<p>Sensors connected to Valo’s smart street lights can monitor trash bins to measure fill-levels. Cities can use this information to plan waste collection schedules and routes, saving time and money. Valo’s Smart City platform is ideal for deploying waste management systems.</p>
+			</div>
+		</div>
+	</div>
+	</main>
+	<!-- #main -->
+	</div>
+	<!-- #primary -->
+
 <?php get_footer(); ?>
