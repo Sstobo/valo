ef5e43f4ea60d1149b818a726fab5b4aa0e6bac4
Colin MJ <colinmj@telus.net>
2018-04-10 16:24:00 -0700

    Contact page 95%done

--- page-contact.php
+++ page-contact.php
@@ -79,8 +79,24 @@ get_header(); ?>
 
 			<?php endwhile; // End of the loop. ?>
 			</section> <!-- #content-wrapper -->
+			<div class="signup-wrapper">
+						<div class="signup-container">
+					<h1 id="form-title"> Sign Up for the Latest Valo News </h1>
+					<?php echo do_shortcode('[contact-form-7 id="4" title="Contact form 1"]');?>
+				</div>
+			</div>
+
+		<div class="contact-footer">
+			<img src="<?php echo get_template_directory_uri() . '/assets/Logo SVG/VALO white logo-01.png'?>" id="contact-logo"/>
+			<p id="contact-copy"> &copy Valo Smart City 2018. All Rights Reserved.</p> 
+		</div>
+
 		</main><!-- #main -->
+
+		
 	</div><!-- #primary -->
 
 
-<?php get_footer(); ?>
+
+
+
