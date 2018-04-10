dd604da3a6bf285179ac4e01bba359b815d0c190
NCMoseley <ncmoseley@gmail.com>
2018-04-10 09:24:18 -0700

    Add styles to about team

--- about_team.php
+++ about_team.php
@@ -12,7 +12,8 @@ get_header(); ?>
 			<section class="content-wrapper">
 					<?php while (have_posts()) : the_post(); ?>
 					<div class="management-team-container">
-					<h1>Managemment Team pages</h1>
+					<h2>Management Team</h2>
+					<div class="title-blue-line"></div>
 					</div>
 						<?php get_template_part('template-parts/content', 'page'); ?>
 					  <?php endwhile; // End of the loop.?>
