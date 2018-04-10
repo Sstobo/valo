a707a2f1c10eaef9ea1663c99211979c9046a4aa
NCMoseley <ncmoseley@gmail.com>
2018-04-10 14:01:49 -0700

    Add About Team page

--- about_team.php
+++ about_team.php
@@ -10,13 +10,128 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<section class="content-wrapper">
-					<?php while (have_posts()) : the_post(); ?>
-					<div class="management-team-container">
+					<div class="management-team-title-container">
 					<h2>Management Team</h2>
 					<div class="title-blue-line"></div>
 					</div>
-						<?php get_template_part('template-parts/content', 'page'); ?>
+					<div class="management-team-container">
+						<?php while (have_posts()) : the_post(); ?>
+						<div class="single-team-member-container ike-sutton">
+							<div class="name-and-position">
+							  <h3><strong>Ike Sutton</strong></h3>
+                <span >DIRECTOR + CEO</span>
+							</div>
+							<p>Mr. Sutton has more than 25 years of successful entrepreneurial experience, especially in emerging markets where he has founded and ran several companies. He has held multiple roles as either President, Chief Executive Officer, or Chairman of the Board for numerous companies, including SavWatt USA Inc., Huayue Electronics Inc., Kaymia Global Solutions Inc., and Tarsier Ltd. He holds a Bachelor of Arts degree in Business Administration from Pace University.
+							</p>
+							<div class="contact">
+								<strong>CONTACT<br>
+								</strong>
+								<a ref="#">LINKEDIN</a>
+								<a ref="#">EMAIL</a>
+							</div>
+						</div>
+						<div class="single-team-member-container terrance-dittrich">
+							<div class="name-and-position">
+							  <h3><strong>Terrance Dittrich</strong></h3>
+                <span><strong>CORPORATE TITLE</strong></span>
+							</div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<div class="contact">
+								<strong>CONTACT<br>
+								</strong>
+								<a ref="#">LINKEDIN</a>
+								<a ref="#">EMAIL</a>
+							</div>
+						</div>
+						<div class="single-team-member-container alan-fogleman">
+							<div class="name-and-position">
+							  <h3><strong>Alan Fogleman</strong></h3>
+								<span><strong>CORPORATE TITLE</strong></span>
+							</div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<div class="contact">
+								<strong>CONTACT<br>
+								</strong>
+								<a ref="#">LINKEDIN</a>
+								<a ref="#">EMAIL</a>
+							</div>
+						</div>
+						<div class="single-team-member-container steve-altman">
+							<div class="name-and-position">
+							  <h3><strong>Steve Altman</strong></h3>
+                <span><strong>CORPORATE TITLE</strong></span>
+							</div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<div class="contact">
+								<strong>CONTACT<br>
+								</strong>
+								<a ref="#">LINKEDIN</a>
+								<a ref="#">EMAIL</a>
+							</div>
+						</div>
+						<div class="single-team-member-container david-harper">
+							<div class="name-and-position">
+							  <h3><strong>David Harper</strong></h3>
+                <span><strong>CORPORATE TITLE</strong></span>
+							</div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<div class="contact">
+								<strong>CONTACT<br>
+								</strong>
+								<a ref="#">LINKEDIN</a>
+								<a ref="#">EMAIL</a>
+							</div>
+						</div>
+						<div class="single-team-member-container wanda-halpert">
+							<div class="name-and-position">
+							  <h3><strong>Wanda Halpert</strong></h3>
+                <span><strong>CORPORATE TITLE</strong></span>
+							</div>
+							<p>Wanda S. Halpert is the President of Concord Business Development where she has completed 700+ business plans for startups and public companies that have raised more than $1.4 Billion in investment capital. Since 1997, Concord Business Development’s plans have been well received by bankers, investors, large and small capital companies, shareholders, auditors, securities counsel, brokers, and investor relations professionals.
+							</p>
+							<div class="contact">
+								<strong>CONTACT<br>
+								</strong>
+								<a ref="#">LINKEDIN</a>
+								<a ref="#">EMAIL</a>
+							</div>
+						</div>
+						<div class="single-team-member-container julio-ferreira">
+							<div class="name-and-position">
+							  <h3><strong>Julio Ferreira</strong></h3>
+                <span><strong>CORPORATE TITLE</strong></span>
+							</div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<div class="contact">
+								<strong>CONTACT<br>
+								</strong>
+								<a ref="#">LINKEDIN</a>
+								<a ref="#">EMAIL</a>
+							</div>
+						</div>
+						<div class="single-team-member-container tim-davranov">
+							<div class="name-and-position">
+							  <h3><strong>Tim Davranov</strong></h3>
+                <span><strong>CORPORATE TITLE</strong></span>
+							</div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+							<div class="contact">
+								<strong>CONTACT<br>
+								</strong>
+								<a ref="#">LINKEDIN</a>
+								<a ref="#">EMAIL</a>
+							</div>
+						</div>
+					</div>
 					  <?php endwhile; // End of the loop.?>
+						<div class="signup-image">
+						</div>
 			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
