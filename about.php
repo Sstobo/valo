a9772255c02e90376943b75e8c180b34b1fb127e
NCMoseley <ncmoseley@gmail.com>
2018-04-11 13:54:07 -0700

    Add styles to About

--- about.php
+++ about.php
@@ -12,14 +12,28 @@ get_header(); ?>
 			<section class="content-wrapper">
 					<div class="management-company-container">
 							<div class="top-row-container">
-									<div class="single-item">
-							 			 <h3><strong>The VALO Vision</strong></h3>
+									<div class="single-item-vision">
+							 			 <h2><strong>The VALO Vision</strong></h2>
+											<p>VALO strives to improve people’s lives by providing better lighting and safer environments. Valo’s Smart City platform makes cities more efficient by collecting real-time data for city services and infrastructure, such as transportation, utilities, security, and pollution.
+											</p>
 									</div>
-									<p>VALO strives to improve people’s lives by providing better lighting and safer environments. Valo’s Smart City platform makes cities more efficient by collecting real-time data for city services and infrastructure, such as transportation, utilities, security, and pollution.
-									</p>
-									<div class="placeholder">
-									<!-- <div class="placeholder">
-									</div> -->
+									<div class="top-row-image">
+									    <div class="vision-image">
+											</div>
+									</div>
+							</div>
+							<div class="our-purpose-container">
+								<div class="our-purpose-image">
+									    <div class="purpose-image">
+											</div>
+									</div>
+									<div class="single-item-purpose">
+							 			 <h2><strong>Our Purpose</strong></h2>
+											<div class="purpose-blue-line"></div>
+											<p>VALO is a smart city integrator that aims to better people’s lives around the world through the Internet of Things (IoT) technology. VALO’s products and services provide better lighting for city inhabitants which increases safety.</p>
+											<p>Additionally, VALO makes cities more efficient by collecting real-time information for city assets, such as traffic, security, pollution, and municipal utilities.</p>
+											<h3>Have a Smart City Project or Idea?</h3>
+											<a href="<?php echo esc_url(home_url('about/#!/contact')); ?>"><button class="button-main button-with-shadow">GET STARTED WITH VALO</button></a>
 									</div>
 							</div>
 					</div>
