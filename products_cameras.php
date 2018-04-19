2769430cc8fbf6fc16a78c02221f9282a6e5ebbb
Colin MJ <colinmj@telus.net>
2018-04-18 18:17:43 -0700

    Completed product software

--- products_cameras.php
+++ products_cameras.php
@@ -11,6 +11,13 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<section class="content-wrapper">
 
+				<!-- parallax top banner -->
+				<div class="camera-banner">
+					<h2>Smart Cameras + Software</h2>
+					<p><span class="uppercase">Valo</span> offers Internet Protocol (IP) cameras that integrate with its Smart City platform. These cameras have a wide variety of applications including people or vehicle monitoring and counting, intrusion and threat detection, loitering detection, and identifying abandoned objects.</p>
+				</div>
+				<!-- close camera banner -->
+
 				<section class="products-two-main-cameras-wrapper">
 					<h3>VALO's two main IP cameras
 						<br> have the following set of features:</h3>
@@ -46,6 +53,86 @@ get_header(); ?>
 
 				</section>
 				<!-- products-two-main-cameras-wrapper -->
+
+				<!-- video control banner -->
+				<section class="video-control">
+					<h2>Video Control</h2>
+					<p><span class="uppercase">Valo's</span> video control software acts as a central platform for managing cameras and videos. This intelligent and powerful Video Management Software (VMS) supports most cameras, and has a wide selection of advanced features and add-ons, allowing for highly-efficient video monitoring and surveillance. Features of the video controls include:</p>
+				</section>
+
+				<!-- close video control banner -->
+
+				<section class="controls">
+					<div class="intuitive">
+					<img alt="Power button" src="<?php echo get_template_directory_uri() . '/assets/camera-software/intelligent-power.png'?>" />
+						<h3>Intuitive + User Friendly</h3>
+						<p>Interface allows users to easily understand the configuration process and start using the software</p>
+					</div>
+
+					<div class="event">
+						<img alt="Power button" src="<?php echo get_template_directory_uri() . '/assets/camera-software/cctv.png'?>" />
+						<h3>Event + Action Management</h3>
+						<p>Automated video surveillance process with an alarm and action manger</p>
+
+					</div>
+
+					<div class="core">
+						<img alt="Power button" src="<?php echo get_template_directory_uri() . '/assets/camera-software/processing-power.png'?>" />
+						<h3>64-Bit Core</h3>
+						<p>Use the full power of system hardware</p>
+					</div>
+
+					<div class="failover">
+						<img alt="Power button" src="<?php echo get_template_directory_uri() . '/assets/camera-software/cpu.png'?>" />
+						<h3>Failover</h3>
+						<p>Continuous monitoring of server status and failover management from the central server</p>
+
+					</div>
+
+					<div class="ready">
+						<img alt="Power button" src="<?php echo get_template_directory_uri() . '/assets/camera-software/digital-camera.png'?>" />
+						<h3>4k Ready</h3>
+						<p>4K ready VMS application with 4K recording and playback</p>
+
+					</div>
+
+					<div class="archive">
+						<img alt="Power button" src="<?php echo get_template_directory_uri() . '/assets/camera-software/public-safety.png'?>" />
+						<h3>Archive Replication</h3>
+						<p>Advanced mirror recording with scheduled transfers to ensure safety of data at all times</p>
+
+					</div>
+
+
+					<div class="powerful">
+						<img alt="Power button" src="<?php echo get_template_directory_uri() . '/assets/camera-software/open-data.png'?>" />
+						<h3>Powerful Analytics</h3>
+						<p>Modules for advanced video analytics and face recognition</p>
+
+
+					</div>
+
+					<div class="integration">
+						<img alt="Power button" src="<?php echo get_template_directory_uri() . '/assets/camera-software/automation.png'?>" />
+						<h3>Integration + API</h3>
+						<p>Combine video surveillance with third party software with integration possibilities</p>
+					</div>
+
+				</section>
+
+				<div class="facial">
+
+					<div class="facial-image">
+						
+					</div>
+
+					<div class="facial-text">
+						<h2>Facial Recognition</h2>
+						<p>Valo supplies face recognition software which is designed to work with video controls. This biometric application provides system performance and reliability.</p>
+
+							<p>The facial recognition software has live face-detection, simultaneous multiple face recognition, and fast face matching in one-to-one and one-to-many modes. This software is idea for security monitoring and law enforcement applications.</p>
+					</div>
+				</div>
 
 			</section>
 			<!-- #content-wrapper -->
