36f56ffe10181f4780c190b2aecf1ad2320750b0
Scott <scottdlivingstone@gmail.com>
2018-05-03 12:28:19 -0700

    Add js and css for modal mail-list signup form

--- investors.php
+++ investors.php
@@ -19,13 +19,53 @@ get_header(); ?>
 			<p>VALO transforms any street light into a cost-effective modular platform for integrated smart city services at no upfront cost to the city. To learn more about VALO and its business objectives, download our investor presentation below.</p>
 			<div class="investors-right-mobile"></div>
 			<h3>Learn More with </br>Our Corporate Presentation</h3>	
-				<a href="<?php echo get_page_link( get_page_by_title( Download )->ID ); ?>"class="button-main button-with-shadow button-align-left-desktop" id="banner-contact-button">Download Now</a>
-				
+				<a href="#0" class="button-main button-with-shadow button-align-left-desktop" id="download-presentation">Download Now</a>
 					</div>
 					<div class="investors-right"></div>
 				</div>
 			</div>
 
+<!-- Begin MailChimp Signup Form -->
+<section class="modal modal-closed">
+	<div class="form-container">
+		
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+	<a class='close-modal' href="#0">X</a>
+<form action="https://na.us18.list-manage.com/subscribe/post?u=b85d0a32099200e4253f57646&amp;id=552579a489" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<h2>Subscribe to our mailing list</h2>
+<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+<div class="mc-field-group input-group">
+    <strong>Select Language </strong>
+    <ul><li><input type="radio" value="1" name="group[401]" id="mce-group[401]-401-0"><label for="mce-group[401]-401-0">First Language</label></li>
+<li><input type="radio" value="2" name="group[401]" id="mce-group[401]-401-1"><label for="mce-group[401]-401-1">Second Language</label></li>
+<li><input type="radio" value="4" name="group[401]" id="mce-group[401]-401-2"><label for="mce-group[401]-401-2">Third Language</label></li>
+</ul>
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b85d0a32099200e4253f57646_552579a489" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
+</div>
+</section>
 		</main>
 		<!-- #main -->
 	</div>
